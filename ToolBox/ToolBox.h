198841c64ee2e617e6971f6e559122e3c25c890f
Pusca, Ghenadie <ghenadie.pusca@garmin.com>
2019-03-25 09:35:19 +0200

    Initial Commit

---
+++ ToolBox/ToolBox.h
@@ -0,0 +1,19 @@
+//
+//  ToolBox.h
+//  ToolBox
+//
+//  Created by Pusca, Ghenadie on 25/03/2019.
+//  Copyright © 2019 Pusca, Ghenadie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToolBox.
+FOUNDATION_EXPORT double ToolBoxVersionNumber;
+
+//! Project version string for ToolBox.
+FOUNDATION_EXPORT const unsigned char ToolBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToolBox/PublicHeader.h>
+
+
